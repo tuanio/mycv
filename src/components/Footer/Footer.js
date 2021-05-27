fa1bd1b2ad0f4177a19ce5fba5b8c85d8cc1ff14
TuanNguyenVanAnh <nvatuan3@gmail.com>
2021-05-27 12:41:40 +0700

    The fucking nice commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,16 @@
 // import Grid from "@material-ui/core/Grid";
 // import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         width: "100%",
         height: 50
+    },
+    text: {
+        textAlign: 'center',
+        padding: '0.5em 0 0 0'
     }
 }));
 
@@ -18,7 +22,9 @@ export default function Footer(props) {
     return (
         <>
             <Paper className={classes.root} elevation={2}>
-                
+                <Typography variant="p" component="p" className={classes.text}>
+                    Made by Tuan with ♥ and greate enthusiasm
+                </Typography>
             </Paper>
         </>
     )
